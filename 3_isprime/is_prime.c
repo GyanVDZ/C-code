fccd0383c8dcc8e1fc686815d14b93a91293120f
Boyan Lowies <lowiesboyan@gmail.com>
2022-10-18 16:47:43 +0200

    Made files teacher proof

--- 3_isprime/is_prime.c
+++ 3_isprime/is_prime.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
-int main(void){
-    int number;
-    printf("Is this a prime number: ");
-    scanf("%d", &number);
-    
-    
-    if(number < 2){
-        printf("%d is not a prime number",number);
-        return 0;
-    }
+/*
+Student: Boyan Lowies
+Studynumber: 5845750
+Assignment number: 3
+*/
 
+
+
+
+int main(void){
+    int number, i;
+	int result = 1;												/*initialise vars*/
+    scanf("%d", &number);										/*get number*/
     
-    for( int i =2 ; (i*i) <= number; i++){
-        if((number % i) == 0){
-            printf("\n%d is not a prime number",number);
-            return 0;
-        }
+    if(number < 2){												
+        result = 0;												/*If number is lower then 2 set result to false*/
+    }
+  
+    for(i =2 ; (i*i) <= number; i++){								
+        if((number % i) == 0){									/*Check if the rest of division number/i is 0*/
+			result = 0;											/*if zero then set result to 0*/
+			break;
+        }	
     } 
-    printf("\n%d is a prime number", number);
-    return 0;
+
+	if(result == 1){											/*check if result is 1 or 0*/
+		printf("%d is a prime number\n",number);
+	}
+	else{
+		printf("%d is not a prime number\n",number);
+	}
+	return 0;
 }
