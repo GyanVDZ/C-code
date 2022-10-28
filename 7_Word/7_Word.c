38ea9db7204ce2de9b2d2972ac514449e73100c1
Boyan Lowies <lowiesboyan@gmail.com>
2022-10-28 09:28:24 +0200

    gfgh

--- 7_Word/7_Word.c
+++ 7_Word/7_Word.c
@@ -3,28 +3,31 @@
 
 int check_string(char string[]){
     char test[] = "let";
-    int lenc = strlen(test), lent = strlen(string), x,y;
+    char hulp[strlen(test)];
+    int lenc = strlen(test), lent = strlen(string), x,y, ret =0;
     for(x=0; (x+lenc-1)<lent; x++){
         for(y=0; y<lenc; y++){
-            printf("%c", string[x+y]);
+            hulp[y] = string[x+y];
         }
-        printf("\n");
+        if(strcmp(hulp, test) == 0){
+            ret++;
+        }
     }
-    
-
-
-
-
+    return(ret);
 }
 
-int main(void){
+int main(int argc, char **argv){
     char string[1024];
     int x, count =0;
+    printf("%s", argv);
 
     while(1){
         scanf("%s", &string);
         count += check_string(string);
-        printf("%d\n", count);
+        if(strcmp("#EOF", string)==0){
+            break;
+        }
     }
+    printf("%d", count);
     return 0;     
 }
