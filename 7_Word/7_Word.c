9cbfd4897a4cf5aee3274dfb5503ab8a95eebdb0
Boyan Lowies <lowiesboyan@gmail.com>
2022-11-01 15:23:56 +0100

    Fixed issue with timing of steps and initialised chars propperly

--- 7_Word/7_Word.c
+++ 7_Word/7_Word.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
-int check_string(char string[]){
-    char test[] = "let";
-    char hulp[strlen(test)];
+/*
+Student: Boyan Lowies
+Studynumber: 5845750
+Assignment number: 7
+*/
+
+int check_string(char string[], char test[]){
+    char sub[1024] = "\0";
     int lenc = strlen(test), lent = strlen(string), x,y, ret =0;
     for(x=0; (x+lenc-1)<lent; x++){
         for(y=0; y<lenc; y++){
-            hulp[y] = string[x+y];
+            sub[y] = string[x+y];
         }
-        if(strcmp(hulp, test) == 0){
+        if(strcmp(sub, test) == 0){
             ret++;
         }
     }
     return(ret);
 }
 
-int main(int argc, char **argv){
-    char string[1024];
-    int x, count =0;
-    printf("%s", argv);
 
+
+int main(int argc, char* argv[]){
+    char string[1024] = "\0";
+    int count =0;
+    
+    if(argc == 1){
+        printf("Please specify a program argument.\n");
+        return 0;
+    }
+    
     while(1){
-        scanf("%s", &string);
-        count += check_string(string);
+        scanf("%s", string);
         if(strcmp("#EOF", string)==0){
             break;
         }
+        count += check_string(string, argv[1]);
     }
-    printf("%d", count);
+    
+    printf("%d\n", count);
     return 0;     
 }
